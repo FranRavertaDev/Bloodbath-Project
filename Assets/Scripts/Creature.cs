4fd25b146ab50f7cad08735c1e19a2d15332afff
FranRavertaDev <fran.r.capua@gmail.com>
2023-02-15 23:59:47 -0300

    Add character asset with animation
    Two different characters with animations to use while learning.

---
+++ Assets/Scripts/Creature.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Creature : MonoBehaviour
+{
+    // Base Stats
+    public int baseStrength;
+    public int baseDexterity;
+    public int baseIntelligence;
+    public int baseConstitution;
+
+    // Base Markers
+    public int baseMaxHealth;
+    public int baseHealth;
+    public int baseMinHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
